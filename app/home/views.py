d568b4a388a24242d41c53b540ad03539fb47b6b
evday <1194984735@qq.com>
2018-01-07 20:45:36 +0800

    登陆，注册页面搭建

--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 #date:"2018-01-07,17:09"
-
+from flask import redirect,render_template
 from . import home
 
 @home.route("/")
 def index():
     return "<h1 style='color:green'>this is home</h1>"
+
+@home.route("/login")
+def login():
+    return render_template("home/login.html")
+
+@home.route("/logout")
+def logout():
+    return redirect("home.login")
+
+@home.route("/register")
+def register():
+    return render_template ("home/regist.html")
+
